4b4c31744501145355a5442b565519b287eaf00b
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-12-19 11:13:59 +0100

    new

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
-
+import closeIcon from "../../assets/nav/closeIcon.png";
+import menuIcon from "../../assets/nav/menuIcon.png";
 
 export default function Navbar() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className={styles.navbarBg}>
     <div className={styles.navbar}>
       <a className={styles.title} href="/">SWEET & SALTY</a>
       <div className={styles.menu}>
-        <ul className={styles.menuItems}>
+        <img className={styles.menuBtn} 
+        src={menuOpen 
+            ? closeIcon
+            : menuIcon
+        } 
+        alt="menu-button"
+        onClick={() => setMenuOpen(!menuOpen)} />
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+        onClick={() => setMenuOpen(false)}>
             <li> <a href={'../../CreatePage'}>Create New Donut</a></li>
             <li> <a href={'../../ListPage'}>All Donuts</a></li>
             <li> <a href={'../../FavouritesPage'}>Favourites</a></li>
