223f20d801aae9f4986a173824d5b904c9fd3ed3
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-04-02 19:45:00 +0200

    new style

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,8 @@ export default function Home() {
 
   return (
       <div className={styles.container}>
+      <h1>Create your perfect Donut</h1>
+      <p>Design, organize, and save your favorite donuts in one place. Mix flavors, add toppings, and build your ultimate collection of sweet creations.</p>
         <div className={styles.btnSectionHome}>
           <button className={styles.btnCard}>
             <Link className={styles.btnLink} href={'../../CreatePage'}>CREATE NEW DONUT</Link>
